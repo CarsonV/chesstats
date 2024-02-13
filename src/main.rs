3d8edb951d335dbd6bebd2ca2b6ddca192f41e69
CarsonV <carsonv@Carsons-MBP.domain>
2024-02-13 16:48:12 -0600

    WIP Testing Inital DB

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{postgres::PgPoolOptions, types::chrono};
 
 #[derive(Debug, Deserialize)]
 struct Inner {
@@ -18,13 +18,36 @@ struct Resp {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() -> Result<(), sqlx::Error> {
 
-    let mainRes = get_lichess();
-    
-    
-    println!("{mainRes:#?}");
+    let pool = PgPoolOptions::new()
+        .max_connections(2)
+        .connect("postgres://postgres:1234@localhost/postgres").await?;
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool).await?;
 
+    sqlx::migrate!("./dbMigrations")
+        .run(&pool)
+        .await?;
+
+    assert_eq!(row.0, 150);
+
+    let res = get_lichess().await.expect("Lichess failed");
+    
+    // https://stackoverflow.com/questions/61561165/how-do-i-define-a-datetime-field-in-sqlx-rust
+    // need to enable adding a timestamptz item using chrono
+    println!("{res:#?}");
+    sqlx::query("INSERT INTO fishnet VALUES ($1, $2, $3, $4, $5, $6)")
+        .bind(chrono::DateTime<chrono::Utc>)
+        .bind(res.analysis.user.acquired)
+        .bind(res.analysis.user.queued)
+        .bind(res.analysis.user.oldest)
+        .bind(res.analysis.system.acquired)
+        .bind(res.analysis.system.queued)
+        .bind(res.analysis.system.oldest)
+        .execute(&pool)
+        .await?;
     Ok(())
     
 }
