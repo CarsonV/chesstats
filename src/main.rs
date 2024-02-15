3fc3a2f487659868fd6e484af633b0e4df7d13e7
CarsonV <carsonv@Carsons-MBP.domain>
2024-02-15 16:13:24 -0600

    Inital task scheduling added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
-use sqlx::{postgres::PgPoolOptions, types::chrono};
+use sqlx::{postgres::PgPoolOptions, PgPool, types::chrono};
+use tokio::time;
 
 #[derive(Debug, Deserialize)]
 struct Inner {
@@ -17,6 +18,7 @@ struct Resp {
     analysis: Outer,
 }
 
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
 
@@ -26,30 +28,32 @@ async fn main() -> Result<(), sqlx::Error> {
     let row: (i64,) = sqlx::query_as("SELECT $1")
         .bind(150_i64)
         .fetch_one(&pool).await?;
+    assert_eq!(row.0, 150);
 
     sqlx::migrate!("./dbMigrations")
         .run(&pool)
         .await?;
-
-    assert_eq!(row.0, 150);
-
-    let res = get_lichess().await.expect("Lichess failed");
     
-    // https://stackoverflow.com/questions/61561165/how-do-i-define-a-datetime-field-in-sqlx-rust
-    // need to enable adding a timestamptz item using chrono
-    println!("{res:#?}");
-    sqlx::query("INSERT INTO fishnet VALUES ($1, $2, $3, $4, $5, $6)")
-        .bind(chrono::DateTime<chrono::Utc>)
-        .bind(res.analysis.user.acquired)
-        .bind(res.analysis.user.queued)
-        .bind(res.analysis.user.oldest)
-        .bind(res.analysis.system.acquired)
-        .bind(res.analysis.system.queued)
-        .bind(res.analysis.system.oldest)
-        .execute(&pool)
-        .await?;
+
+    let mut interval = time::interval(time::Duration::from_secs(60));
+    for _i in 0..3 {
+        interval.tick().await;
+        handle_stats(&pool).await?;
+    }
+    
+    
     Ok(())
     
+}
+
+async fn handle_stats(db: &PgPool) -> Result<(), sqlx::Error> {
+
+    let res = get_lichess().await.expect("Lichess failed");
+    println!("{res:#?}");
+    write_data(&db, &res).await.expect("DB write failed");
+
+    Ok(())
+
 }
 
 async fn get_lichess() -> Result<Resp, reqwest::Error> {
@@ -59,4 +63,27 @@ async fn get_lichess() -> Result<Resp, reqwest::Error> {
         .await?;
 
     Ok(res)
+}
+async fn write_data(db: &PgPool, res: &Resp) -> Result<(), sqlx::Error> {
+    sqlx::query("INSERT INTO fishnet VALUES ($1, $2, $3, $4, $5, $6, $7)")
+        .bind(chrono::Utc::now())
+        .bind(res.analysis.user.acquired)
+        .bind(res.analysis.user.queued)
+        .bind(res.analysis.user.oldest)
+        .bind(res.analysis.system.acquired)
+        .bind(res.analysis.system.queued)
+        .bind(res.analysis.system.oldest)
+        .execute(db)
+        .await?;
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
 }
